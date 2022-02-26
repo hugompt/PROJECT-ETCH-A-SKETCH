5f84e7e486872b047cd8a98ab21b9cee13786bfb
hugompt <hugomsfh@hotmail.com>
2022-02-27 00:57:41 +0100

    CHANGELOG:  - Created the grid 16x16 using a function on windows.load  - Added a border to the grid to make it more visually appealing

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-function createGrid(){
-    let container = document.getElementsByClassName("gridCont");
-    let cell = document.createElement("div");
-    cell.textContent = "TEXT";
-    container.appendChild(cell);
+//Function to create the default grid with JS
+function defaultGrid(){
+    let columns = 16;
+    let rows = 16;
+
+    for (let i = 0; i < rows; i++) {
+        let row = document.createElement('div');
+        row.id = "cellRow_" + i;
+        row.className = "row";
+        for (let j = 0; j < columns; j++) {
+            let column = document.createElement('div');
+            column.id = "cellColumn_" + j;
+            column.className = "cell";
+            row.appendChild(column);
+        }              
+        document.getElementById('mainGrid').appendChild(row);
+    }
+
 }
 
+
+
 window.onload = function(){
-    createGrid();
+    defaultGrid();
 }
+
