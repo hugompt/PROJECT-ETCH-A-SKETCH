7bc093f866e81a707b669bdff4bf9b2a05275f27
hugompt <hugomsfh@hotmail.com>
2022-02-27 03:08:23 +0100

    CHANGELOG - Added the footer - Added a erase button to clear the grid - On mouseclick and mousehover changes the color of the cells to black

--- main.js
+++ main.js
@@ -19,23 +19,37 @@ function defaultGrid(){
 }
 
 
-
+//On load of the page call a function to create the grid
 window.onload = function(){
-    defaultGrid();
-
-    // $(function() {
-    //     $('.cell').hover( function(){
-    //        $(this).css('background-color', 'black');
-    //     },
-    //     function(){
-    //        $(this).css('background-color', 'white');
-    //     });
-    //  });
-
-     let timeoutId = 0;
-     $('.cell').on('mousedown', function() {
-        $(this).css('background-color', 'black');
-    });
-     
+    defaultGrid();   
 }
+
+//Check is page is ready to run code
+$(document).ready(function(){
+    //Create variable check if the user is holding down the mouse
+    let mouseDown = false;
+    $(document).on('mousedown', function() {
+        mouseDown = true;
+    });
+    $(document).on('mouseup', function(){
+        mouseDown = false;
+    });
+
+    //When the user clicks on a cell, change color to black
+    $('.cell').on('mousedown', ( function (){
+        $(this).css('background-color', 'black');
+    }));
+
+    //When a user hovers a cell with the mouse clicked,
+    //change the color to black
+    $('.cell').hover( function(){
+        if(mouseDown){
+            $(this).css('background-color', 'black');
+        }
+    });
+
+    $('.eraser').on('mousedown', (function(){
+        $('.cell').css('background-color', 'white');
+    }));
+})
 
